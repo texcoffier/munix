f09cff899d2801ba1b02c73fc453742387342258
Thierry EXCOFFIER <thierry.excoffier@univ-lyon1.fr>
2015-03-04 20:11:21 +0100

    Test the parsing of all the permutation of special characters

---
+++ colorize_fuzzing.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+"""
+Try to parse every permutation of the special characters
+"""
+
+import math
+import itertools
+import time
+import colorize
+
+print "Start very long test"
+
+chars = set((colorize.list_stopper
+            + colorize.redirection_stopper
+            + colorize.argument_stopper
+             + "`").replace("\t", "a1'\""))
+
+nb = 0
+last = 0
+def check(txt, depth):
+    if depth == 0:
+        try:
+            colorize.Parser(txt).parse()
+        except KeyboardInterrupt:
+            raise
+        except:
+            print "BUG for:", txt
+        return
+    depth -= 1
+    for char in chars:
+        check(txt + char, depth)
+for i in range(1, len(chars)):
+    print time.ctime(), "check length:", i
+    check("", i)
+
+print "OK"
